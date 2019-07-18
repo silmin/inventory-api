66275f3bebc79f94ad5cd977b30f209237198e0b
silmin <silvestmint@gmail.com>
2019-07-19 03:24:11 +0900

    add: view products

--- view/getProducts.js
+++ view/getProducts.js
@@ -1,7 +1,14 @@
-var getProducts = $(function() {
-    $.get("https://silmin-inventory.herokuapp.com/api.php/", function(data) {
-        console.log(data);
-    });
-});
+function getProducts() {
+    let json;
+    $.get("https://silmin-inventory.herokuapp.com/api.php", function(data) {
+         json = JSON.parse(data);
+    })
+        .done(function() {
+            console.log('done');
+            reflectProducts(json);
+        })
+        .fail(function() {
+            console.log('fail');
+        });
+}
 
-getProducts();
